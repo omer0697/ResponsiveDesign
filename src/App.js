da61ae349c372188ccea51a18cf06a373d713094
omer9706 <omer_yurtseven1997@outlook.com>
2022-10-18 21:05:14 +0300

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Find from "./components/Find/Find";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/navbar/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navbar/>
+     <Hero/>
+     <Find/>
     </div>
   );
 }
